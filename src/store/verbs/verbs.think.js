3efe63decc50537b8e18c9a88811a4582d235a22
Artem Tsys <19successful91@gmail.com>
2023-06-13 17:03:07 +0300

    remove cach

--- src/store/verbs/verbs.think.js
+++ src/store/verbs/verbs.think.js
@@ -3,12 +3,12 @@ import getAxiosInstance from '../../api/axios-instanse';
 
 export const fetchVerbs = createAsyncThunk('verbs/fetchVerbs', async () => {
   const data = await getAxiosInstance().get('/verbs').then(res => {
-      const result = JSON.stringify(res)
-      const resultData = JSON.stringify(res.data)
+      const result = JSON.stringify(res.config)
+      // const resultData = JSON.stringify(res.data)
       const resultStatus = JSON.stringify(res.status)
       alert(`resultStatus: ${resultStatus}`)
       alert(`result: ${result}`)
-      alert(`resultData: ${resultData}`)
+      // alert(`resultData: ${resultData}`)
       return res.data;
     }).catch((err) => {
       alert(`error: ${err} - ${err.statusText}`)
