c7f3635ee458b6c9008b49ae323e8f5195cdc25b
Artem Tsys <19successful91@gmail.com>
2023-06-13 16:06:19 +0300

    add alerts

--- src/store/verbs/verbs.think.js
+++ src/store/verbs/verbs.think.js
@@ -1,16 +1,16 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 
 export const fetchVerbs = createAsyncThunk('verbs/fetchVerbs', async () => {
-  const data = await fetch( `${process.env.PUBLIC_URL}/verbs`)
-    .then(response => {
-      if (!response.ok) {
+  const data = await fetch( `${process.env.PUBLIC_URL}/verbs`).then(res => {
+      if (!res.ok) {
         throw new Error('Network response was not OK');
       }
-      console.log('response', response);
-      return response.json();
+      return res.json();
+    }).then(res => {
+      return res
     })
     .catch((err) => {
-      alert(`error: ${JSON.stringify(err)}`)
+      alert(`error: ${err.status} - ${err.message}`)
       throw new Error(err.message)
     })
   alert(JSON.stringify(data));
